ff71c2347a201eb70e4d0e1b545de903ca8eb83c
Tsutsui Tatsumi <tsutsuitatsumi@Tsutsuis-MacBook-Air.local>
2024-03-08 11:40:31 +0900

    外部ライブラリを利用した簡易アプリ

---
+++ sample.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const message = "hello world";
+console.log(message);
+const lodash_1 = require("lodash");
+const array = [1, 2, 3, 4, 5];
+const total = (0, lodash_1.sum)(array);
+console.log("Sum of array:", total);
---
+++ sample.ts
@@ -0,0 +1,10 @@
+
+
+const message:string = "hello world";
+console.log(message);
+
+import { sum as lodashSum } from 'lodash';
+
+const array = [1, 2, 3, 4, 5];
+const total = lodashSum(array);
+console.log("Sum of array:", total);
