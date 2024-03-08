51bb98214561824cfc8029cbe2a56a0ecd699d08
Tsutsui Tatsumi <tsutsuitatsumi@Tsutsuis-MacBook-Air.local>
2024-03-08 11:48:43 +0900

    importの位置調整

--- sample.ts
+++ sample.ts
@@ -1,9 +1,7 @@
-
+import { sum as lodashSum } from 'lodash';
 
 const message:string = "hello world";
 console.log(message);
-
-import { sum as lodashSum } from 'lodash';
 
 const array = [1, 2, 3, 4, 5];
 const total = lodashSum(array);
